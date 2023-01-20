4b5b5f5432bde4b5b3e5f6c439641072143ea5e7
Ryan <dashbuilds@gmail.com>
2023-01-20 14:44:56 +1030

    now connected to the databse
    json has been deleted

--- application.py
+++ application.py
@@ -3,8 +3,28 @@ import json
 from datetime import datetime
 from collections import Counter
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 application = Flask(__name__)
+
+application.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ClimbingDB:PostgresPassword@awseb-e-uxswatm7qi-stack-awsebrdsdatabase-1nfndsualofh.ctt3bhz1abz7.ap-southeast-2.rds.amazonaws.com:5432/ebdb"
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(application)
+
+class Climb(db.Model):
+    __tablename__ = 'Climbs'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    location = db.Column(db.String(10), nullable=False)
+    black = db.Column(db.Integer, nullable=False)
+    white = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, black, white, location, date):
+        self.black = black
+        self.white = white
+        self.location = location
+        self.date = date
 
 @application.route('/')
 def index():
@@ -13,21 +33,17 @@ def index():
 
 @application.route('/climbing', methods=['POST'])
 def climbing():
+
     date = request.form['date']
     white = int(request.form['white'])
     black = int(request.form['black'])
     location = request.form['location']
     climbing = {'date': date, 'white': white, 'black': black, 'location': location}
 
-    try:
-        with open('climbing.txt', 'r') as f:
-            data = json.load(f)
-    except:
-        data = []
-
-    data.append(climbing)
-    with open('climbing.txt', 'w') as f:
-        json.dump(data, f)
+    #trying to add to the database
+    climb = Climb(white=white, black=black, location=location, date=date)
+    db.session.add(climb)
+    db.session.commit()
 
     today = datetime.now().strftime("%Y-%m-%d")
     return render_template('index.html', today=today, success=True)
@@ -35,27 +51,27 @@ def climbing():
 
 @application.route('/data', methods=['GET', 'POST'])
 def data():
+    #delete the climb if needed
     if request.method == 'POST':
         if 'delete_climb' in request.form:
-            date = request.form['delete_climb']
-            with open('climbing.txt', 'r') as f:
-                climbing = json.load(f)
-            climbing = [climb for climb in climbing if climb['date'] != date]
-            with open('climbing.txt', 'w') as f:
-                json.dump(climbing, f)
-            
-    with open('climbing.txt', 'r') as f:
-        climbing = json.load(f)
+            deleteID = request.form['delete_climb']
+            climb_to_delete = Climb.query.filter_by(id=deleteID).first()
+            db.session.delete(climb_to_delete)
+            db.session.commit()
 
-    white = [climb['white'] for climb in climbing]
-    black = [climb['black'] for climb in climbing]
-    locations = [climb['location'] for climb in climbing]
-    
-    avg_white = round(sum(white) / len(white), 1)
-    avg_black = round(sum(black) / len(black), 1)
-    most_common_location = Counter(locations).most_common(1)[0][0]
 
-    return render_template('data.html', climbing=climbing, avg_white=avg_white, avg_black=avg_black, most_common_location=most_common_location)
+    #query the database
+    allClimbs = Climb.query.all()    
+    location_list = db.session.query(Climb.location).all()
+    location_list = [location[0] for location in location_list]
+
+    #calculate statistics
+    avg_white = round(db.session.query(func.avg(Climb.white)).scalar(), 1)
+    avg_black = round(db.session.query(func.avg(Climb.black)).scalar(), 1)
+    location_counts = Counter(location_list)
+    most_common_location = location_counts.most_common(1)[0][0]
+
+    return render_template('data.html', climbing=allClimbs, avg_white=avg_white, avg_black=avg_black, most_common_location=most_common_location)
 
 if __name__ == '__main__':
     application.run()
